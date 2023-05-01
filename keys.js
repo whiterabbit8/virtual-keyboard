ad7d6ed9bd389b2a461776fb89e7b782f674ae3d
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-05-01 10:47:18 +0300

    feat: create keyboard

---
+++ keys.js
@@ -0,0 +1,146 @@
+const keys = [
+  // 1st row
+  {
+    code: 'Backquote', en: '`', enShift: '~', ru: 'ё', ruShift: 'Ё', row: 0, place: 0,
+  }, {
+    code: 'Digit1', en: '1', enShift: '!', ru: '1', ruShift: '!', row: 0, place: 1,
+  }, {
+    code: 'Digit2', en: '2', enShift: '@', ru: '2', ruShift: '"', row: 0, place: 2,
+  }, {
+    code: 'Digit3', en: '3', enShift: '#', ru: '3', ruShift: '№', row: 0, place: 3,
+  }, {
+    code: 'Digit4', en: '4', enShift: '$', ru: '4', ruShift: ';', row: 0, place: 4,
+  }, {
+    code: 'Digit5', en: '5', enShift: '%', ru: '5', ruShift: '%', row: 0, place: 5,
+  }, {
+    code: 'Digit6', en: '6', enShift: '^', ru: '6', ruShift: ':', row: 0, place: 6,
+  }, {
+    code: 'Digit7', en: '7', enShift: '&', ru: '7', ruShift: '?', row: 0, place: 7,
+  }, {
+    code: 'Digit8', en: '8', enShift: '*', ru: '8', ruShift: '*', row: 0, place: 8,
+  }, {
+    code: 'Digit9', en: '9', enShift: '(', ru: '9', ruShift: '(', row: 0, place: 9,
+  }, {
+    code: 'Digit0', en: '0', enShift: ')', ru: '0', ruShift: ')', row: 0, place: 10,
+  }, {
+    code: 'Minus', en: '-', enShift: '_', ru: '-', ruShift: '_', row: 0, place: 11,
+  }, {
+    code: 'Equal', en: '=', enShift: '+', ru: '=', ruShift: '+', row: 0, place: 12,
+  }, {
+    code: 'Backspace', isSpecial: true, name: 'backspace', class: 'backspace', row: 0, place: 13,
+  },
+
+  // 2nd row
+  {
+    code: 'Tab', isSpecial: true, name: 'tab', class: 'tab', row: 1, place: 0,
+  }, {
+    code: 'KeyQ', en: 'q', enShift: 'Q', ru: 'й', ruShift: 'Й', row: 1, place: 1,
+  }, {
+    code: 'KeyW', en: 'w', enShift: 'W', ru: 'ц', ruShift: 'Ц', row: 1, place: 2,
+  }, {
+    code: 'KeyE', en: 'e', enShift: 'E', ru: 'у', ruShift: 'У', row: 1, place: 3,
+  }, {
+    code: 'KeyR', en: 'r', enShift: 'R', ru: 'к', ruShift: 'К', row: 1, place: 4,
+  }, {
+    code: 'KeyT', en: 't', enShift: 'T', ru: 'е', ruShift: 'Е', row: 1, place: 5,
+  }, {
+    code: 'KeyY', en: 'y', enShift: 'Y', ru: 'н', ruShift: 'Н', row: 1, place: 6,
+  }, {
+    code: 'KeyU', en: 'u', enShift: 'U', ru: 'г', ruShift: 'Г', row: 1, place: 7,
+  }, {
+    code: 'KeyI', en: 'i', enShift: 'I', ru: 'ш', ruShift: 'Ш', row: 1, place: 8,
+  }, {
+    code: 'KeyO', en: 'o', enShift: 'O', ru: 'щ', ruShift: 'Щ', row: 1, place: 9,
+  }, {
+    code: 'KeyP', en: 'p', enShift: 'P', ru: 'з', ruShift: 'З', row: 1, place: 10,
+  }, {
+    code: 'BracketLeft', en: '[', enShift: '{', ru: 'х', ruShift: 'Х', row: 1, place: 11,
+  }, {
+    code: 'BracketRight', en: ']', enShift: '}', ru: 'ъ', ruShift: 'Ъ', row: 1, place: 12,
+  }, {
+    code: 'Backslash', en: '\\', enShift: '|', ru: '\\', ruShift: '/', row: 2, place: 13,
+  }, {
+    code: 'Delete', isSpecial: true, name: 'del', class: 'del', row: 2, place: 14,
+  },
+
+  // 3rd row
+  {
+    code: 'CapsLock', isSpecial: true, name: 'capslock', class: 'caps', row: 2, place: 0,
+  }, {
+    code: 'KeyA', en: 'a', enShift: 'A', ru: 'ф', ruShift: 'Ф', row: 2, place: 1,
+  }, {
+    code: 'KeyS', en: 's', enShift: 'S', ru: 'ы', ruShift: 'Ы', row: 2, place: 2,
+  }, {
+    code: 'KeyD', en: 'd', enShift: 'D', ru: 'в', ruShift: 'В', row: 2, place: 3,
+  }, {
+    code: 'KeyF', en: 'f', enShift: 'F', ru: 'а', ruShift: 'А', row: 2, place: 4,
+  }, {
+    code: 'KeyG', en: 'g', enShift: 'G', ru: 'п', ruShift: 'П', row: 2, place: 5,
+  }, {
+    code: 'KeyH', en: 'h', enShift: 'H', ru: 'р', ruShift: 'Р', row: 2, place: 6,
+  }, {
+    code: 'KeyJ', en: 'j', enShift: 'J', ru: 'о', ruShift: 'О', row: 2, place: 7,
+  }, {
+    code: 'KeyK', en: 'k', enShift: 'K', ru: 'л', ruShift: 'Л', row: 2, place: 8,
+  }, {
+    code: 'KeyL', en: 'l', enShift: 'L', ru: 'д', ruShift: 'Д', row: 2, place: 9,
+  }, {
+    code: 'Semicolon', en: ';', enShift: ':', ru: 'ж', ruShift: 'Ж', row: 2, place: 10,
+  }, {
+    code: 'Quote', en: "'", enShift: '"', ru: 'э', ruShift: 'Э', row: 2, place: 11,
+  }, {
+    code: 'Enter', isSpecial: true, name: 'enter', class: 'enter', row: 2, place: 13,
+  },
+
+  // 4th row
+  {
+    code: 'ShiftLeft', isSpecial: true, name: 'shift', class: 'shift shift_left', row: 3, place: 0,
+  }, {
+    code: 'KeyZ', en: 'z', enShift: 'Z', ru: 'я', ruShift: 'Я', row: 3, place: 1,
+  }, {
+    code: 'KeyX', en: 'x', enShift: 'X', ru: 'ч', ruShift: 'Ч', row: 3, place: 2,
+  }, {
+    code: 'KeyC', en: 'c', enShift: 'C', ru: 'с', ruShift: 'С', row: 3, place: 3,
+  }, {
+    code: 'KeyV', en: 'v', enShift: 'V', ru: 'м', ruShift: 'М', row: 3, place: 4,
+  }, {
+    code: 'KeyB', en: 'b', enShift: 'B', ru: 'и', ruShift: 'И', row: 3, place: 5,
+  }, {
+    code: 'KeyN', en: 'n', enShift: 'N', ru: 'т', ruShift: 'Т', row: 3, place: 6,
+  }, {
+    code: 'KeyM', en: 'm', enShift: 'M', ru: 'ь', ruShift: 'Ь', row: 3, place: 7,
+  }, {
+    code: 'Comma', en: ',', enShift: '<', ru: 'б', ruShift: 'Б', row: 3, place: 8,
+  }, {
+    code: 'Period', en: '.', enShift: '>', ru: 'ю', ruShift: 'Ю', row: 3, place: 9,
+  }, {
+    code: 'Slash', en: '/', enShift: '?', ru: '.', ruShift: ',', row: 3, place: 10,
+  }, {
+    code: 'ArrowUp', isSpecial: true, name: '↑', class: 'arrow arrow_up', row: 3, place: 11,
+  }, {
+    code: 'ShiftRight', isSpecial: true, name: 'shift', class: 'shift shift_right', row: 3, place: 12,
+  },
+
+  // 5th row
+  {
+    code: 'ControlLeft', isSpecial: true, name: 'ctrl', class: 'ctrl ctrl_left', row: 4, place: 0,
+  }, {
+    code: 'AltLeft', isSpecial: true, name: 'alt', class: 'alt alt_left', row: 4, place: 1,
+  }, {
+    code: 'MetaLeft', isSpecial: true, name: 'win', class: 'win win_left', row: 4, place: 2,
+  }, {
+    code: 'Space', isSpecial: true, name: '', class: 'space', row: 4, place: 3,
+  }, {
+    code: 'MetaRight', isSpecial: true, name: 'alt', class: 'alt alt_right', row: 4, place: 4,
+  }, {
+    code: 'ArrowLeft', isSpecial: true, name: '←', class: 'arrow arrow_left', row: 4, place: 5,
+  }, {
+    code: 'ArrowDown', isSpecial: true, name: '↓', class: 'arrow arrow_down', row: 4, place: 6,
+  }, {
+    code: 'ArrowRight', isSpecial: true, name: '→', class: 'arrow arrow_right', row: 4, place: 7,
+  }, {
+    code: 'AltRight', isSpecial: true, name: 'ctrl', class: 'ctrl ctrl_right', row: 4, place: 8,
+  },
+];
+
+export default keys;
